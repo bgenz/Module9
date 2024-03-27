f6b5184af79ecc437e0a9f1eb25cd5646f96c59e
bgenz <98568574+bgenz@users.noreply.github.com>
2024-03-26 20:18:16 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='Demo'
+  version='42'
+  description='In-class simple package development.'
+  url='https://github.com/bgenz/Module9/new/main/Armaments'
+  license='GNU'
+  packages=['Armaments']
+)
